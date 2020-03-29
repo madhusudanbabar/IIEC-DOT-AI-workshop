994269767ec94df39634b9561b0b1d49aff8fa78
Krypton <madhusudanbabar@gmail.com>
2020-03-29 13:31:44 +0530

    getting column in python

---
+++ post.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 29 10:30:09 2020
+
+@author: krypton
+"""
+
+
+#%% 
+
+myList = [["madhu", 9999],
+          ["krypton", 2222],
+          ["impulse", 1111]]
+
+print(myList[:,1])
+
+ans = [ row[1] for row in myList]
+
